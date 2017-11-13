5813323ac6cd94e9064065303a58750947740b4f
L-A-Davis <lindsay.amanda.davis@gmail.com>
2017-11-13 17:52:49 -0500

    completed NYC pigeon organizer

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,18 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+    output = {}
+  data.each do |data_point, hash|
+      hash.each do |key, value|
+        value.each do |item|
+            if output.key?(item) && output[item].key?(data_point)
+                output[item][data_point] << key.to_s
+            elsif output.key?(item)
+                output[item][data_point] = [key.to_s]
+            else
+               output[item] = {}
+               output[item][data_point] = [key.to_s]
+            end
+         end
+       end
+      end
+  output
 end
